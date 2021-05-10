190e989dfa2d189b34b0185e64fd2c81774d46ef
Ian306 <66493808+Ian306@users.noreply.github.com>
2021-05-10 00:39:25 -0500

    Add project files.

---
+++ Tarea4DWBE/Controllers/CustomerController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Tarea4DWBE.DTO;
+using Tarea4DWBE.Models;
+
+namespace Tarea4DWBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly NORTHWNDContext DBContext;
+
+        public CustomerController(NORTHWNDContext DBContext)
+        {
+            this.DBContext = DBContext;
+        }
+
+        [HttpGet("GetCustomers")]
+        public async Task<ActionResult<List<CustomerDTO>>> Get()
+        {
+            var List = await DBContext.Customers.Select(
+                s => new CustomerDTO
+                {
+                    CustomerId = s.CustomerId,
+                    CompanyName = s.CompanyName,
+                    ContactName = s.ContactName,
+                    ContactTitle = s.ContactTitle,
+                    Address = s.Address, 
+                    City = s.City, 
+                    Region = s.Region, 
+                    PostalCode = s.PostalCode, 
+                    Country = s.Country, 
+                    Phone = s.Phone, 
+                    Fax = s.Fax,
+                    //Orders = s.Orders, 
+                    //CustomerCustomerDemos = s.CustomerCustomerDemos, 
+
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+
+        [HttpGet("GetCustomerById")]
+        public async Task<ActionResult<CustomerDTO>> GetCustomerById(string Id)
+        {
+            CustomerDTO Customer = await DBContext.Customers.Select(
+                    s => new CustomerDTO
+                    {
+                        CustomerId = s.CustomerId,
+                        CompanyName = s.CompanyName,
+                        ContactName = s.ContactName,
+                        ContactTitle = s.ContactTitle,
+                        Address = s.Address,
+                        City = s.City,
+                        Region = s.Region,
+                        PostalCode = s.PostalCode,
+                        Country = s.Country,
+                        Phone = s.Phone,
+                        Fax = s.Fax,
+                        //Orders = s.Orders,
+                        //CustomerCustomerDemos = s.CustomerCustomerDemos,
+                    })
+                .FirstOrDefaultAsync(s => s.CustomerId == Id);
+
+            if (Customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Customer;
+            }
+        }
+
+        [HttpPost("InsertCustomer")]
+        public async Task<HttpStatusCode> InsertCustomer(CustomerDTO Customer)
+        {
+            var newCustomer = new Customer()
+            {
+                CustomerId = Customer.CustomerId,
+                CompanyName = Customer.CompanyName,
+                ContactName = Customer.ContactName,
+                ContactTitle = Customer.ContactTitle,
+                Address = Customer.Address,
+                City = Customer.City,
+                Region = Customer.Region,
+                PostalCode = Customer.PostalCode,
+                Country = Customer.Country,
+                Phone = Customer.Phone,
+                Fax = Customer.Fax,
+                //Orders = Customer.Orders,
+                //CustomerCustomerDemos = Customer.CustomerCustomerDemos,
+            };
+
+            DBContext.Customers.Add(newCustomer);
+            await DBContext.SaveChangesAsync();
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPut("UpdateCustomer")]
+        public async Task<HttpStatusCode> UpdateCustomer(CustomerDTO Customer)
+        {
+            var CustomerToUpdate = await DBContext.Customers.FirstOrDefaultAsync(s => s.CustomerId
+            == Customer.CustomerId);
+
+            CustomerToUpdate.CustomerId = Customer.CustomerId;
+            CustomerToUpdate.CompanyName = Customer.CompanyName;
+            CustomerToUpdate.ContactName = Customer.ContactName;
+            CustomerToUpdate.ContactTitle = Customer.ContactTitle;
+            CustomerToUpdate.Address = Customer.Address;
+            CustomerToUpdate.City = Customer.City;
+            CustomerToUpdate.Region = Customer.Region;
+            CustomerToUpdate.PostalCode = Customer.PostalCode;
+            CustomerToUpdate.Country = Customer.Country;
+            CustomerToUpdate.Phone = Customer.Phone;
+            CustomerToUpdate.Fax = Customer.Fax;
+            //CustomerToUpdate.Orders = Customer.Orders;
+            //CustomerToUpdate.CustomerCustomerDemos = Customer.CustomerCustomerDemos;
+
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+
+        [HttpDelete("DeleteCustomer/{Id}")]
+        public async Task<HttpStatusCode> DeleteCustomer(string Id)
+        {
+            var CustomerToDelete = new Customer()
+            {
+                CustomerId = Id
+            };
+            DBContext.Customers.Attach(CustomerToDelete);
+            DBContext.Customers.Remove(CustomerToDelete);
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ Tarea4DWBE/Controllers/EmployeeController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Tarea4DWBE.DTO;
+using Tarea4DWBE.Models;
+
+namespace Tarea4DWBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly NORTHWNDContext DBContext;
+
+        public EmployeeController(NORTHWNDContext DBContext)
+        {
+            this.DBContext = DBContext;
+        }
+
+        [HttpGet("GetEmployees")]
+        public async Task<ActionResult<List<EmployeeDTO>>> Get()
+        {
+            var List = await DBContext.Employees.Select(
+                s => new EmployeeDTO
+                {
+                    EmployeeId = s.EmployeeId, 
+                    LastName = s.LastName, 
+                    FirstName = s.FirstName, 
+                    Title = s.Title, 
+                    TitleOfCourtesy = s.TitleOfCourtesy, 
+                    Address = s.Address, 
+                    City = s.City,
+                    Region = s.Region, 
+                    PostalCode = s.PostalCode, 
+                    Country = s.Country,
+                    HomePhone = s.HomePhone,
+                    Notes = s.Notes,
+                    ReportsTo = s.ReportsTo,
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+
+        [HttpGet("GetEmployeeById")]
+        public async Task<ActionResult<EmployeeDTO>> GetEmployeeById(int Id)
+        {
+            EmployeeDTO Employee = await DBContext.Employees.Select(
+                    s => new EmployeeDTO
+                    {
+                        EmployeeId = s.EmployeeId,
+                        LastName = s.LastName,
+                        FirstName = s.FirstName,
+                        Title = s.Title,
+                        TitleOfCourtesy = s.TitleOfCourtesy,
+                        Address = s.Address,
+                        City = s.City,
+                        Region = s.Region,
+                        PostalCode = s.PostalCode,
+                        Country = s.Country,
+                        HomePhone = s.HomePhone,
+                        Notes = s.Notes,
+                        ReportsTo = s.ReportsTo,
+                    })
+                .FirstOrDefaultAsync(s => s.EmployeeId == Id);
+
+            if (Employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Employee;
+            }
+        }
+
+        [HttpPost("InsertEmployee")]
+        public async Task<HttpStatusCode> InsertEmployee(EmployeeDTO Employee)
+        {
+            var newEmployee = new Employee()
+            {
+                EmployeeId = Employee.EmployeeId,
+                LastName = Employee.LastName,
+                FirstName = Employee.FirstName,
+                Title = Employee.Title,
+                TitleOfCourtesy = Employee.TitleOfCourtesy,
+                Address = Employee.Address,
+                City = Employee.City,
+                Region = Employee.Region,
+                PostalCode = Employee.PostalCode,
+                Country = Employee.Country,
+                HomePhone = Employee.HomePhone,
+                Notes = Employee.Notes,
+                ReportsTo = Employee.ReportsTo,
+            };
+
+            DBContext.Employees.Add(newEmployee);
+            await DBContext.SaveChangesAsync();
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPut("UpdateEmployee")]
+        public async Task<HttpStatusCode> UpdateEmployee(EmployeeDTO Employee)
+        {
+            var EmployeeToUpdate = await DBContext.Employees.FirstOrDefaultAsync(s => s.EmployeeId
+            == Employee.EmployeeId);
+
+            EmployeeToUpdate.EmployeeId = Employee.EmployeeId;
+            EmployeeToUpdate.LastName = Employee.LastName;
+            EmployeeToUpdate.FirstName = Employee.FirstName;
+            EmployeeToUpdate.Title = Employee.Title;
+            EmployeeToUpdate.TitleOfCourtesy = Employee.TitleOfCourtesy;
+            EmployeeToUpdate.Address = Employee.Address;
+            EmployeeToUpdate.City = Employee.City;
+            EmployeeToUpdate.Region = Employee.Region;
+            EmployeeToUpdate.PostalCode = Employee.PostalCode;
+            EmployeeToUpdate.Country = Employee.Country;
+            EmployeeToUpdate.HomePhone = Employee.HomePhone;
+            EmployeeToUpdate.Notes = Employee.Notes;
+            EmployeeToUpdate.ReportsTo = Employee.ReportsTo;
+
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+
+        [HttpDelete("DeleteEmployee/{Id}")]
+        public async Task<HttpStatusCode> DeleteEmployee(int Id)
+        {
+            var EmployeeToDelete = new Employee()
+            {
+                EmployeeId = Id
+            };
+            DBContext.Employees.Attach(EmployeeToDelete);
+            DBContext.Employees.Remove(EmployeeToDelete);
+            await DBContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+    }
+}
